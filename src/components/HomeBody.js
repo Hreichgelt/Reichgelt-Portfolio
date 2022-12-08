594fb53f1e79f61e2ba6d03d0918a431a83b727f
Hans Reichgelt <hreichgelt06@gmail.com>
2022-12-08 10:29:48 -0600

    red notice for migration to GH pages

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './styles/homebody.css'
 import { Link } from 'react-router-dom'
+import Banner from './Banner';
 
 
 const Header = () => {
     return (
     <div className='homebody'>
+        <Banner />
         <div className='content'>
             <h1>Hans Reichgelt's Portfolio</h1>
             <div>
